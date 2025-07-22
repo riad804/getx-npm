6b3b590cebf77bfa495c675b232ab05f644aecec
riad804 <developer.riad@gmail.com>
2025-07-22 12:59:36 +0600

    remove unnecessary folders

--- dist/dependency.d.ts
+++
@@ -1,6 +0,0 @@
-import { GetXController } from "./controller";
-export declare function put<T extends GetXController>(instance: T, { permanent }?: {
-    permanent?: boolean;
-}): T;
-export declare function find<T extends GetXController>(type: new () => T): T;
-export declare function deleteInstance<T extends GetXController>(type: new () => T): void;
