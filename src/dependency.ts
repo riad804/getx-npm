7e2e6929cb19badfeab60e38ad055cbf2d73cac6
riad804 <developer.riad@gmail.com>
2025-07-22 22:10:36 +0600

    update readme

--- src/dependency.ts
+++ src/dependency.ts
@@ -2,87 +2,41 @@
 
 import {GetXController} from "./controller";
 
-type Constructor<T = any> = new (...args: any[]) => T;
-type InstanceRecord = {
-    instance: any;
-    permanent: boolean;
-    scope?: string;
-};
+const _instances = new Map<string, any>();
 
-const _instances = new Map<string, InstanceRecord>();
-const _scopes = new Map<string, Map<string, InstanceRecord>>();
-
-export class DependencyInjector {
-    static put<T extends GetXController>(
-        instance: T,
-        options: { permanent?: boolean; scope?: string } = {}
-    ): T {
-        const key = instance.constructor.name;
-        const record = <InstanceRecord>{ instance, ...options };
-
-        if (options.scope) {
-            // Scoped instance
-            if (!_scopes.has(options.scope)) {
-                _scopes.set(options.scope, new Map());
-            }
-            _scopes.get(options.scope)?.set(key, record);
-        } else {
-            // Global instance
-            _instances.set(key, record);
-        }
-
+export function put<T extends GetXController>(
+    instance: T,
+    { permanent = false }: { permanent?: boolean } = {}
+): T {
+    const key = instance.constructor.name;
+    if (!_instances.has(key)) {
         instance.initialize();
-        return instance;
-    }
-
-    static find<T extends GetXController>(
-        type: Constructor<T>,
-        scope?: string
-    ): T {
-        const key = type.name;
-
-        if (scope) {
-            // Look for scoped instance
-            const scopedInstances = _scopes.get(scope);
-            if (scopedInstances?.has(key)) {
-                return scopedInstances.get(key)?.instance as T;
+        if (!permanent) {
+            // Auto dispose when component unmounts
+            if (typeof window !== 'undefined') {
+                window.addEventListener('beforeunload', () => {
+                    instance.dispose();
+                });
             }
         }
-
-        // Look for global instance
-        if (_instances.has(key)) {
-            return _instances.get(key)?.instance as T;
-        }
-
-        // Create new instance if not found
-        const newInstance = new type();
-        this.put(newInstance, { scope });
-        return newInstance;
+        _instances.set(key, instance);
     }
-
-    static delete(type: Constructor, scope?: string) {
-        const key = type.name;
-
-        if (scope) {
-            _scopes.get(scope)?.get(key)?.instance.dispose();
-            _scopes.get(scope)?.delete(key);
-        } else {
-            _instances.get(key)?.instance.dispose();
-            _instances.delete(key);
-        }
-    }
-
-    static clearScope(scope: string) {
-        const instances = _scopes.get(scope);
-        if (instances) {
-            instances.forEach(record => record.instance.dispose());
-            _scopes.delete(scope);
-        }
-    }
+    return _instances.get(key) as T;
 }
 
-// Shortcut functions
-export const put = DependencyInjector.put;
-export const find = DependencyInjector.find;
-export const deleteInstance = DependencyInjector.delete;
-export const clearScope = DependencyInjector.clearScope;
+export function find<T extends GetXController>(type: new () => T): T {
+    const key = type.name;
+    if (!_instances.has(key)) {
+        put(new type());
+    }
+    return _instances.get(key) as T;
+}
+
+export function deleteInstance<T extends GetXController>(type: new () => T) {
+    const key = type.name;
+    if (_instances.has(key)) {
+        const instance = _instances.get(key) as T;
+        instance.dispose();
+        _instances.delete(key);
+    }
+}
